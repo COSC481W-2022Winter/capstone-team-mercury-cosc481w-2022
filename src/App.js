9943368d47dbc63897702a3913a46388a203e43f
wrathbun <wrathbun@emich.edu>
2022-02-23 11:07:34 -0500

    feat: Added logout button
    Committing in place of Mitchell who is having issues with git
    Adds a new logout button component, will clear the user's session variable and return them to the default route (home page/login page) after clicking
    resolves issue #3

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
-import Feed from './pages/Feed.js';
+import Login from './pages/login.js';
+import Feed from './pages/feed.js';
+import Profile from './pages/profile';
 import Navigation from './components/Navigation';
+import NewPost from './pages/newPost.js';
+import Register from './pages/register.js';
  
 class App extends Component {
   render() {
     return (      
        <BrowserRouter>
         <div>
-          <Navigation />
+          <Profile />
             <Routes>
-             <Route path="/" element={<Home />} exact/>
+             <Route path="/" element={<Login />} exact/>
              <Route path="/about" element={<About />}/>
              <Route path="/contact" element={<Contact />}/>
+             <Route path="/feed" element={<Feed />}/>
+             <Route path="/signup" element={<Register />}/>
              <Route element={<Error />}/>
            </Routes>
-		      <Feed />
         </div> 
       </BrowserRouter>
     );
