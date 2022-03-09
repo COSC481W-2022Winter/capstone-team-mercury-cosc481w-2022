ac8067d964093e509fbc7ce4d278d9da246d9442
wrathbun <wrathbun@emich.edu>
2022-03-09 15:19:20 -0500

    feat: Update user schema
    Updated the user schema to include 3 new fields for use in profile customization; name, bio, and website.
    All are type: String and not required
    Resolves my part of #20

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    name: {
+    name: { //newly added for profile customization
         type: String
     },
     bio: {
