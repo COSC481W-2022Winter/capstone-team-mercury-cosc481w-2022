7c224fc75cb47e86342f651705ab58e212ee0fe5
mley1 <47510116+mley1@users.noreply.github.com>
2022-03-18 14:43:14 -0400

    Write additional api instructions
    Created the API for getting all comments for a particular post as well as adding a new comment.

--- backend/api/postAPI.js
+++ backend/api/postAPI.js
@@ -17,13 +17,15 @@ router.post("/newPost", function(req, res) {
 	var poster = req.body.username+"";
     var text = req.body.content+"";
 	var attachments = ""; //TBA
+    var comments = [""];
 	console.log("Entering a new post by " +poster);
 	console.log(text);
 
     //add post to the database
     const post = new Post({
 		postedBy: poster,
-        content: text
+        content: text,
+        comments: []
         //attachments: [] TBA
     });
     post.save().then((result) => {
@@ -34,4 +36,37 @@ router.post("/newPost", function(req, res) {
         console.log(err);
     });
 });
+
+// Pull all comments for a specific
+// post from the database
+
+router.post("/getAllComments", function(req,res){
+
+    console.log("Getting all comments for post: "+post);
+
+    res.send(comments);
+
+    console.log(comment);
+});
+
+
+
+
+// Attach a new comment to a post
+
+router.post("/writeNewComment", function(req,res){
+
+	var commenter = req.body.username+"";
+    var comment = req.body.content+"";
+
+    post.save().then((result) => {
+        console.log(result);
+        console.log("Success!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+});
+
 module.exports = router;
