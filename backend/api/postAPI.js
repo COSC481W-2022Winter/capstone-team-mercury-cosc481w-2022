0b50b493cb454a82b1665b0e64196793277536f0
JosephTech <49079887+JosephTech@users.noreply.github.com>
2022-03-17 22:13:59 -0400

    feat: Uploading Images
    When uploading a file, it will only accept up to three images and the
    file type must be 'image'. Included error message when there is more
    than three images as well as when there is no file or text in the post.
    If there is more than one image the images will be displayed in a
    carousel
    
    Resolves: Issue #19

--- backend/api/postAPI.js
+++ backend/api/postAPI.js
@@ -14,21 +14,22 @@ router.use(bodyParser.urlencoded({ extended: true }));
 //puts a new post into the database
 router.post("/newPost", function(req, res) {
     //get post data
-	var poster = req.body.username+"";
-    var text = req.body.content+"";
-	var attachment1 = req.body.attachments;
-    var attachment2 = null; //replace with the names you're sending
-    var attachment3 = null;
-	console.log("Entering a new post by " +poster);
-	console.log(text);
-    console.log(attachment1);
+	var poster = req.body.username + "";
+    var text = req.body.content + "";
+    var attachments = req.body.attachments;
+    var postData = { postedBy: poster };
+    console.log("Entering a new post by " + poster);
+    if (text) {
+        postData.content = text;
+        console.log(text);
+    }
+    if (attachments) {
+        postData.attachments = attachments;
+        console.log(attachments);
+    }
 
     //add post to the database
-    const post = new Post({
-		postedBy: poster,
-        content: text,
-        attachments: [attachment1, attachment2, attachment3]
-    });
+    const post = new Post(postData);
     post.save().then((result) => {
         console.log(result);
         console.log("Success!");
