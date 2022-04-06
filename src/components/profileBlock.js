364ce5e22bf0914d45935a0460b7c982b5ae62d2
mkanza <91054294+mkanza@users.noreply.github.com>
2022-04-06 00:03:03 -0400

    feat: added added pfp and banner to profile block
    Added support for the profile picture and banner image to the profile block

--- src/components/profileBlock.js
+++ src/components/profileBlock.js
@@ -1,27 +1,52 @@
-import React, { Component } from 'react';
-import { ReactSession } from 'react-client-session';
-import FollowButton from './followButton';
-import pagecss from '../pages/page.css';
-import Logout from './logout';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { ReactSession } from "react-client-session";
+import pagecss from "../pages/page.css";
+import Logout from "./logout";
+import { NavLink } from "react-router-dom";
 
 class profileBLock extends Component {
-	constructor(props) {
-        super();
-        this.state = {
-          user: props.user,
-          align: props.align
-        };
-      }
+  constructor(props) {
+    super();
+    this.state = {
+      user: props.user,
+      align: props.align,
+    };
+  }
 
-    render() {
-        return (
-        <div className='profile' style={ this.state.align === "center"? {margin: "auto"} : {float: this.state.align}}>
-            <h2>{this.state.user.username}</h2>
-            <h3>{this.state.user.name}</h3>
-            <p>{this.state.user.bio}</p>
-            {this.state.user.website !=undefined || this.state.user.website !=""? (<p><a href={this.state.user.website}>Website</a></p>) : null}
-            {(ReactSession.get("username") == this.state.user.username)? (<div><NavLink to="../../editProfile"><input type="submit" value="Edit Profile" /></NavLink> <Logout /> </div>): null}
+  render() {
+    return (
+      <div
+        class="profile"
+        style={
+          this.state.align === "center"
+            ? { margin: "auto" }
+            : { float: this.state.align }
+        }
+      >
+        <div class="card">
+          <div class="ds-top">
+            <img src={this.state.user.bannerurl} alt="Banner"></img>
+          </div>
+          <div class="avatar-holder">
+            <img src={this.state.user.avatarurl} alt="Profile Picture"></img>
+          </div>
+        </div>
+        <div class="profiledata">
+          <h2>{this.state.user.username}</h2>
+          <h3>{this.state.user.name}</h3>
+          <p>{this.state.user.bio}</p>
+          {this.state.user.website != undefined ||
+          this.state.user.website != "" ? (
+            <p>
+              <a href={this.state.user.website}>Website</a>
+            </p>
+          ) : null}
+          {ReactSession.get("username") == this.state.user.username ? (
+            <div>
+              <NavLink to="../../editProfile">
+                <input type="submit" value="Edit Profile" />
+              </NavLink>{" "}
+              <Logout />{" "}
             {!(((ReactSession.get("username") == this.state.user.username) || ReactSession.get("username") == "")) && <FollowButton username={this.state.user.username}/>}
         </div> 
          );
