b9162d332ba29951434c1e5690b72128bd48df94
mkanza <91054294+mkanza@users.noreply.github.com>
2022-04-06 00:05:04 -0400

    feat: Display pfp next to username on posts
    Added the user's profile picture next to their name on posts

--- src/components/post.js
+++ src/components/post.js
@@ -1,56 +1,98 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import pagecss from '../pages/page.css'
-import Comments from './Comments';
-import Tags from './Tags';
-import Likes from './Likes';
-import { Carousel } from 'react-responsive-carousel';
-import '../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import Pagecss from "../pages/page.css";
+import axios from "axios";
+import Comments from "./Comments";
+import Tags from "./Tags";
+import Likes from "./Likes";
+import { Carousel } from "react-responsive-carousel";
+import "../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
 class post extends Component {
-	constructor(props) {
-        super();
-        this.state = {
-          post: props.post
-        };
-      }
+  constructor(props) {
+    super();
+    this.state = {
+      post: props.post,
+      posterProfilePic: ""
+    };
+  }
 
-    carouselImages = (attachments) => {
-        return attachments.map((attachment) => {
-            return (<div> 
-                <img src={attachment} alt="Image Placeholder" width="auto" height="auto"/>
-            </div>);
-        })
-    }
 
-    displayImages = (attachments) => {
-        if (attachments.length > 1) {
-            return (<Carousel showArrows={true} width={500} height={300}>
-                { this.carouselImages(attachments) } 
-            </Carousel>);
-        } else if (attachments.length == 1) {
-            return (<img src={attachments[0]} alt="Image Placeholder" width="500" height="300"/>);
-        }
-        return;
-    }
+  componentDidMount() {
+    axios
+      .post("/api/profileAPI/getUserDetails", {
+        username: this.state.post.postedBy,
+      })
+      .then((response) => {
+        this.setState({
+          posterProfilePic: response.data[0].avatarurl
+        });
+      })
+      .catch(() => {
+        console.log("An error occoured");
+      });
+  }
 
-    //maps each post
-    displayPost = (post) => {
-        return (
-            <div className = "post" style={{margin: "auto"}}>
-                <h4> <NavLink to={'/user/' + post.postedBy}>{post.postedBy}</NavLink></h4> 
-                <p><small>{post.time}</small></p>
-                <p>{post.content}</p>
-                {this.displayImages(post.attachments)}
-                <div><b>Tags: </b><Tags tagList={post.tags} /></div> 
-                <br />
-                <Likes postID={""+post._id} />
-                <Comments postID={""+post._id} />
-            </div>
-        );
-    }
+  carouselImages = (attachments) => {
+    return attachments.map((attachment) => {
+      return (
+        <div>
+          <img
+            src={attachment}
+            alt="Image Placeholder"
+            width="auto"
+            height="auto"
+          />
+        </div>
+      );
+    });
+  };
 
-    render() {
-        return this.displayPost(this.state.post);
+  displayImages = (attachments) => {
+    if (attachments.length > 1) {
+      return (
+        <Carousel showArrows={true} width={500} height={300}>
+          {this.carouselImages(attachments)}
+        </Carousel>
+      );
+    } else if (attachments.length == 1) {
+      return (
+        <img
+          src={attachments[0]}
+          alt="Image Placeholder"
+          width="500"
+          height="300"
+        />
+      );
     }
+    return;
+  };
+
+  //maps each post
+  displayPost = (post) => {
+    return (
+      <div className="post" style={{ margin: "auto" }}>
+        <h4>
+          {" "}
+          <NavLink to={"/user/" + post.postedBy}><img src={this.state.posterProfilePic} alt="Profile Picture" className="pfpOnPosts"></img>{post.postedBy}</NavLink>
+        </h4>
+        <p>
+          <small>{post.time}</small>
+        </p>
+        <p>{post.content}</p>
+        {this.displayImages(post.attachments)}
+        <div>
+          <b>Tags: </b>
+          <Tags tagList={post.tags} />
+        </div>
+        <br />
+        <Likes postID={"" + post._id} />
+        <Comments postID={"" + post._id} />
+      </div>
+    );
+  };
+
+  render() {
+    return this.displayPost(this.state.post);
+  }
 }
 export default post;
