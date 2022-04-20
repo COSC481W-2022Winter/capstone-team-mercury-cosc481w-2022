9078b5bd55169d800abf31f6d69d8f259476156e
mkanza <91054294+mkanza@users.noreply.github.com>
2022-04-20 02:09:53 -0400

    feat: added profile likes component
    Added a component for displaying profile likes on the profile

---
+++ src/components/profileLikes.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Navigation from '../components/Navigation';
+import { NavLink } from 'react-router-dom';
+import { ReactSession } from 'react-client-session';
+
+import Post from './post';
+
+class feed extends Component {
+	constructor(props) {
+        super();
+        
+        this.state = {
+          username: props.username,
+		  postIds: [],
+          posts: []
+        };
+      }
+
+	//retrieves posts whenever component mounts
+	componentDidMount = () => {
+		if( this.state.username != "")
+			this.getPosts();
+	}
+
+	//retrieves our posts from people that are currently followed
+	getPosts = () => {
+		axios.post('/api/userAPI/mostRecentLikePosts', {
+			username: ReactSession.get('username')
+		}) //api route goes here
+		.then((response) => {
+			const data = response.data;
+		  	this.setState({ posts: data.posts.reverse()});
+		})
+		.catch((err) => {
+		  	console.log(err);
+		});
+	}
+	
+	//maps each post
+	displayPosts = (posts) => {
+		//if there are no posts
+		if (!posts.length) return null;
+		return posts.map((post, index) => (
+			<>
+				<Post post={post}><NavLink to={"/post/" + post._id} className="post-link"/></Post>
+				<br/>
+			</>
+		));
+	}
+	
+	//rendering
+	render() {
+		return (  
+			<div className="userRelatedPosts">
+				<h3 style={{textAlign: "center"}}>Liked Posts</h3>
+				{this.displayPosts(this.state.posts)}
+				{this.state.posts.length ==0? <h3 style={{textAlign: "center"}}>This user hasn't liked any posts</h3> : null}
+			</div> 
+		);
+	}
+}
+
+export default feed;
